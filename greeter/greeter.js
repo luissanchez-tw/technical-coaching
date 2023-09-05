dca1e4e222329458a464a35148a04fb2f2b939db
Luis Sánchez Rebollo <luis.sanchezrebollo@thoughtworks.com>
2023-09-05 09:23:36 +0200

    Changes by matteo

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,6 +1,11 @@
 'use strict';
 
 class Greeter {
+    greet(name){
+        const trimmedName = name.trim();
+        const nameCapitalized = trimmedName.charAt(0).toUpperCase() + trimmedName.slice(1);
+        return "Hello " + nameCapitalized;
+    }
 }
 
 module.exports = Greeter;
--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -2,6 +2,36 @@
 
 const Greeter = require('./greeter.js')
 
-test('test runner is set up correctly', () => {
-    expect(0).toBe(1);
+test('Should return Hello Luis given a name Luis', () => {
+    const greeter = new Greeter();
+
+    const result = greeter.greet("Luis");
+
+    expect(result).toBe("Hello Luis");
 });
+
+test('Should return trimmed result',()=>{
+    const greeter = new Greeter();
+
+    const result = greeter.greet(' Luis ');
+
+    expect(result).toBe("Hello Luis");
+});
+
+test('Capitalizes the first letter of the name',()=>{
+    const greeter = new Greeter();
+
+    const result = greeter.greet('matteo');
+
+    expect(result).toBe("Hello Matteo");
+});
+
+test('Capitalizes the first letter of the name and trims',()=>{
+    const greeter = new Greeter();
+
+    const result = greeter.greet('   ariess  ');
+
+    expect(result).toBe("Hello Ariess");
+});
+
+
