cf0df38905c6eb7d0995c2bd88ee0f2ca0589163
Luis Sánchez Rebollo <luis.sanchezrebollo@thoughtworks.com>
2023-09-07 10:44:40 +0200

    L: Added good evening logic

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -10,8 +10,10 @@ class Greeter {
         let greet;
         const trimmedName = name.trim();
         const nameCapitalized = trimmedName.charAt(0).toUpperCase() + trimmedName.slice(1);
-        if (isAfter(currentTime, 6, 0, 0) && !isAfter(currentTime, 12, 0, 0)) {
+        if (isAfter(currentTime, 5, 59, 59) && !isAfter(currentTime, 12, 0, 0)) {
             greet = "Good Morning ";
+        }else if(isAfter(currentTime, 17, 59, 59)&& !isAfter(currentTime, 22, 0, 0)){
+            greet = "Good Evening ";
         } else {
             greet = "Hello ";
         }
--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -7,50 +7,55 @@ const EARLY_MORNING = new Date(2020, 1, 1, 2, 0, 0);
 
 test('Should return Hello Luis given a name Luis', () => {
     const greeter = new Greeter();
-
     const result = greeter.greet("Luis", EARLY_MORNING);
-
     expect(result).toBe("Hello Luis");
 });
 
 test('Should return trimmed result', () => {
     const greeter = new Greeter();
-
     const result = greeter.greet(' Luis ', EARLY_MORNING);
-
     expect(result).toBe("Hello Luis");
 });
 
 test('Capitalizes the first letter of the name', () => {
     const greeter = new Greeter();
-
     const result = greeter.greet('matteo', EARLY_MORNING);
-
     expect(result).toBe("Hello Matteo");
 });
 
 test('Capitalizes the first letter of the name and trims', () => {
     const greeter = new Greeter();
-
     const result = greeter.greet('   ariess  ', EARLY_MORNING);
-
     expect(result).toBe("Hello Ariess");
 });
 
 test('Returns good morning in the correct time', () => {
     const greeter = new Greeter();
     const currentTime = new Date('December 17, 1995 06:24:00');
-
     const result = greeter.greet('Rony', currentTime);
-
     expect(result).toBe("Good Morning Rony");
 });
 
 test('Doesnt Returns good morning in the incorrect time', () => {
     const greeter = new Greeter();
     const currentTime = new Date('December 17, 1995 12:00:01');
-
     const result = greeter.greet('Rony', currentTime);
-
     expect(result).toBe("Hello Rony");
+});
+
+test('Returns Good evening in the correct time', () => {
+    const greeter = new Greeter();
+    const currentTime = new Date('December 17, 1995 18:00:00');
+
+    const result = greeter.greet('Laura', currentTime);
+
+    expect(result).toBe("Good Evening Laura");
+});
+test('Doesnt return Good evening in the incorrect time', () => {
+    const greeter = new Greeter();
+    const currentTime = new Date('December 17, 1995 22:00:01');
+
+    const result = greeter.greet('Laura', currentTime);
+
+    expect(result).toBe("Hello Laura");
 });
