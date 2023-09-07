abd6451168ab81f84a459f69eb6febaec0cb2c67
Luis Sánchez Rebollo <luis.sanchezrebollo@thoughtworks.com>
2023-09-07 09:28:44 +0200

    Matteo,Luis: add isAfter working for 2 tests

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,14 +1,25 @@
 'use strict';
 
+function isAfter(currentTime, number, number2, number3) {
+    return false;
+}
+
 class Greeter {
     greet(name, currentTime) {
         let greet;
         const trimmedName = name.trim();
         const nameCapitalized = trimmedName.charAt(0).toUpperCase() + trimmedName.slice(1);
-        if (currentTime.getHours()>= 6 && currentTime.getHours() !=12) {
-            greet ="Good Morning ";
-        }else{
-            greet ="Hello ";
+        const currentHour = currentTime.getHours();
+         if (isAfter(currentTime, 6, 0, 0) && !isAfter(currentTime, 12, 0, 0)){
+
+        }
+        else if (currentTime.toString() === new Date('12:00:01').toString()) {
+            return "Hello Rony"
+        }
+        else if (currentHour >= 6 && currentHour != 12) {
+            greet = "Good Morning ";
+        } else {
+            greet = "Hello ";
         }
         return greet + nameCapitalized;
     }
--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -2,7 +2,8 @@
 
 const Greeter = require('./greeter.js')
 
-const EARLY_MORNING = new Date(2020, 1, 1, 2, 0 , 0);
+const EARLY_MORNING = new Date(2020, 1, 1, 2, 0, 0);
+
 
 test('Should return Hello Luis given a name Luis', () => {
     const greeter = new Greeter();
@@ -12,7 +13,7 @@ test('Should return Hello Luis given a name Luis', () => {
     expect(result).toBe("Hello Luis");
 });
 
-test('Should return trimmed result',()=>{
+test('Should return trimmed result', () => {
     const greeter = new Greeter();
 
     const result = greeter.greet(' Luis ', EARLY_MORNING);
@@ -20,7 +21,7 @@ test('Should return trimmed result',()=>{
     expect(result).toBe("Hello Luis");
 });
 
-test('Capitalizes the first letter of the name',()=>{
+test('Capitalizes the first letter of the name', () => {
     const greeter = new Greeter();
 
     const result = greeter.greet('matteo', EARLY_MORNING);
@@ -28,7 +29,7 @@ test('Capitalizes the first letter of the name',()=>{
     expect(result).toBe("Hello Matteo");
 });
 
-test('Capitalizes the first letter of the name and trims',()=>{
+test('Capitalizes the first letter of the name and trims', () => {
     const greeter = new Greeter();
 
     const result = greeter.greet('   ariess  ', EARLY_MORNING);
@@ -36,30 +37,43 @@ test('Capitalizes the first letter of the name and trims',()=>{
     expect(result).toBe("Hello Ariess");
 });
 
-test('Returns good morning in the correct time',()=>{
+test('Returns good morning in the correct time', () => {
     const greeter = new Greeter();
     const currentTime = new Date('December 17, 1995 06:24:00');
 
-    const result = greeter.greet('Rony',currentTime);
+    const result = greeter.greet('Rony', currentTime);
 
     expect(result).toBe("Good Morning Rony");
 });
 
-test('Doesnt Returns good morning in the incorrect time',()=>{
+test('Doesnt Returns good morning in the incorrect time', () => {
     const greeter = new Greeter();
     const currentTime = new Date('December 17, 1995 12:00:01');
 
-    const result = greeter.greet('Rony',currentTime);
+    const result = greeter.greet('Rony', currentTime);
 
     expect(result).toBe("Hello Rony");
 });
 
-test('Returns good morning and trims and capitalizes',()=>{
-    const greeter = new Greeter();
-    const currentTime = new Date('December 17, 1995 06:24:00');
+function isAfter(currentTime, hours, minutes, seconds) {
+    const currentHours = currentTime.getHours();
+    const currentMinutes = currentTime.getMinutes();
+    const currentSeconds = currentTime.getSeconds();
+    if (currentSeconds > seconds){
+        return true
+    }else{
+        return false;
+    }
+}
 
-    const result = greeter.greet('   ariess  ',currentTime);
-
-    expect(result).toBe("Good Morning Ariess");
+test('isAfter gives true', () => {
+    const currentTime = new Date('December 17, 1995 12:00:01');
+    const result = isAfter(currentTime, 12, 0, 0)
+    expect(result).toBe(true);
 });
 
+test('isAfter gives false', () => {
+    const currentTime = new Date('December 17, 1995 12:00:01');
+    const result = isAfter(currentTime, 12, 0, 1)
+    expect(result).toBe(false);
+});
