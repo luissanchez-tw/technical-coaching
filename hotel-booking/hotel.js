5817f082397bc8a555cca25be708ff9d46199776
Luis Sánchez Rebollo <luis.sanchezrebollo@thoughtworks.com>
2023-09-18 16:34:37 +0200

    M,L: add bookings array

--- hotel-booking/hotel.js
+++ hotel-booking/hotel.js
@@ -1,24 +1,39 @@
 'use strict';
 
+class Booking {
+    constructor(start,end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
 class Hotel {
 
     constructor() {
-        this.previousStart = undefined;
-        this.previousEnd = undefined;
+        this.bookings = [];
     }
 
     book(startDate, endDate) {
-        if ((startDate > this.previousStart     && startDate < this.previousEnd) ||
-            (endDate > this.previousStart       && endDate < this.previousEnd) ||
-            (startDate === this.previousStart   && endDate === this.previousEnd) ||
-            startDate < this.previousStart      && endDate > this.previousEnd) {
+        if (this.isNotAllowed(startDate, endDate)) {
             return "BOOKING_NOT_ALLOWED";
         } else {
             this.previousStart = startDate;
             this.previousEnd = endDate;
+            const booking = new Booking(startDate, endDate);
+            this.bookings.push(booking);
             return "BOOKING CONFIRMED";
         }
 
+    }
+
+    isNotAllowed(startDate, endDate) {
+        if (this.bookings.length === 0) {
+            return false;
+        }
+        const booking = this.bookings[0];
+        return (startDate >= booking.start && startDate < booking.end) ||
+            (endDate > booking.start && endDate < booking.end) ||
+            startDate < booking.start && endDate >= booking.end;
     }
 }
 
--- hotel-booking/hotel_test.js
+++ hotel-booking/hotel_test.js
@@ -11,69 +11,91 @@ function date(month, day) {
     return new Date(2023, month, day);
 }
 
-test('booking without previous bookings', () => {
-    const startDate = date(10, 1);
-    const endDate = date(10, 2);
+describe("check that we don't allow overlapping bookings", () => {
 
-    const result = hotel.book(startDate, endDate);
+    test('booking without previous bookings', () => {
+        const startDate = date(10, 1);
+        const endDate = date(10, 2);
 
-    expect(result).toBe("BOOKING CONFIRMED");
+        const result = hotel.book(startDate, endDate);
+
+        expect(result).toBe("BOOKING CONFIRMED");
+    });
+
+    test('booking when date is taken', () => {
+        hotel.book(date(10, 1), date(10, 2));
+
+        const result = hotel.book(date(10, 1), date(10, 2));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
+
+    test('booking then new booking right after', () => {
+        hotel.book(date(10, 1), date(10, 2));
+
+        const result = hotel.book(date(10, 2), date(10, 3));
+
+        expect(result).toBe("BOOKING CONFIRMED");
+    });
+
+    test('booking then new booking right before', () => {
+        hotel.book(date(10, 1), date(10, 2));
+
+        const result = hotel.book(date(9, 30), date(10, 1));
+
+        expect(result).toBe("BOOKING CONFIRMED");
+    });
+
+    test('booking overlapping the first night', () => {
+        hotel.book(date(10, 1), date(10, 4));
+
+        const result = hotel.book(date(10, 1), date(10, 2));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
+
+    test('booking overlapping the middle night', () => {
+        hotel.book(date(10, 1), date(10, 4));
+
+        const result = hotel.book(date(10, 2), date(10, 3));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
+
+    test('booking overlapping the last night', () => {
+        hotel.book(date(10, 1), date(10, 4));
+
+        const result = hotel.book(date(10, 3), date(10, 4));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
+
+    test('booking before and after old booking', () => {
+        hotel.book(date(10, 5), date(10, 6));
+
+        const result = hotel.book(date(10, 4), date(10, 7));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
+
+    test('booking before and equal end to an old booking', () => {
+        hotel.book(date(10, 5), date(10, 6));
+
+        const result = hotel.book(date(10, 4), date(10, 6));
+
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
 });
 
-test('booking when date is taken', () => {
-    const startDate = date(10, 1);
-    const endDate = date(10, 2);
-    hotel.book(startDate, endDate);
+describe("when we have many bookings", () => {
 
-    const result = hotel.book(startDate, endDate);
+    xtest('booking when date is taken', () => {
+        hotel.book(date(10, 1), date(10, 2));
+        hotel.book(date(11, 1), date(11, 2));
 
-    expect(result).toBe("BOOKING_NOT_ALLOWED");
-});
+        const result = hotel.book(date(10, 1), date(10, 2));
 
-test('booking then new booking right after', () => {
-    hotel.book(date(10, 1), date(10, 2));
+        expect(result).toBe("BOOKING_NOT_ALLOWED");
+    });
 
-    const result = hotel.book(date(10, 2), date(10, 3));
-
-    expect(result).toBe("BOOKING CONFIRMED");
-});
-
-test('booking then new booking right before', () => {
-    hotel.book(date(10, 1), date(10, 2));
-
-    const result = hotel.book(date(9, 30), date(10, 1));
-
-    expect(result).toBe("BOOKING CONFIRMED");
-});
-
-test('booking overlapping the first night', () => {
-    hotel.book(date(10, 1), date(10, 4));
-
-    const result = hotel.book(date(10, 1), date(10, 2));
-
-    expect(result).toBe("BOOKING_NOT_ALLOWED");
-});
-
-test('booking overlapping the middle night', () => {
-    hotel.book(date(10, 1), date(10, 4));
-
-    const result = hotel.book(date(10, 2), date(10, 3));
-
-    expect(result).toBe("BOOKING_NOT_ALLOWED");
-});
-
-test('booking overlapping the last night', () => {
-    hotel.book(date(10, 1), date(10, 4));
-
-    const result = hotel.book(date(10, 3), date(10, 4));
-
-    expect(result).toBe("BOOKING_NOT_ALLOWED");
-});
-
-test('booking before and after old booking', () => {
-    hotel.book(date(10, 5), date(10, 6));
-
-    const result = hotel.book(date(10, 4), date(10, 7));
-
-    expect(result).toBe("BOOKING_NOT_ALLOWED");
 });
