cf98ee19752c7200920a9cae63cefe4b85efb0ec
Luis Sánchez Rebollo <luis.sanchezrebollo@thoughtworks.com>
2023-09-21 09:47:27 +0200

    M,L: refact calculus of free capacity for a given range

--- hotel-booking/hotel.js
+++ hotel-booking/hotel.js
@@ -1,35 +1,40 @@
 'use strict';
 
 class Booking {
-    constructor(start,end) {
+    constructor(start, end) {
         this.start = start;
         this.end = end;
     }
 
-    isOverlapped(startDate,endDate){
+    isOverlapped(startDate, endDate) {
         return (startDate >= this.start && startDate < this.end) ||
-        (endDate > this.start && endDate < this.end) ||
-        startDate < this.start && endDate >= this.end
+            (endDate > this.start && endDate < this.end) ||
+            startDate < this.start && endDate >= this.end
     }
 }
 
 class Hotel {
     static BOOKING_CONFIRMED = "BOOKING_CONFIRMED";
     static BOOKING_NOT_ALLOWED = "BOOKING_NOT_ALLOWED";
+
     constructor(args) {
         this.bookings = [];
         this.totalCapacity = args.totalCapacity;
     }
 
     book(startDate, endDate) {
-        if (this.totalCapacity - this.getRoomsBookedInRange(startDate,endDate) === 0) {
-            return Hotel.BOOKING_NOT_ALLOWED;
-        }
-        else {
+        if (this.getFreeCapacityForThisRange(startDate, endDate) > 0) {
             const booking = new Booking(startDate, endDate);
             this.bookings.push(booking);
             return Hotel.BOOKING_CONFIRMED;
+        } else {
+            return Hotel.BOOKING_NOT_ALLOWED;
         }
+    }
+
+
+    getFreeCapacityForThisRange(startDate, endDate) {
+        return this.totalCapacity - this.getRoomsBookedInRange(startDate, endDate);
     }
 
     getRoomsBookedInRange(startDate, endDate) {
